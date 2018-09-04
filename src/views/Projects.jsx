f41c3cbaebf0dd3ca05db9d702793c268b1303fb
mihneavlad <mihnel@yahoo.com>
2018-09-04 18:05:05 +0200

    Added cards to the projects view

--- src/views/Projects.jsx
+++ src/views/Projects.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import { Card, Button, CardImg, CardTitle, CardText, CardColumns} from 'reactstrap';
 
-const Projects = (props) => (
-  <p></p>
-)
+const Projects = (props) => {
+  return (
+    <CardColumns>
+      <Card>
+        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" alt="Card image cap" />
+      </Card>
+      <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
+        <CardTitle>Special Title Treatment</CardTitle>
+        <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+        <Button>Button</Button>
+      </Card>
+      <Card>
+        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" alt="Card image cap" />
+      </Card>
+      <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
+        <CardTitle>Special Title Treatment</CardTitle>
+        <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+        <Button>Button</Button>
+      </Card>
+      <Card>
+        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" alt="Card image cap" />
+      </Card>
+      <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
+        <CardTitle>Special Title Treatment</CardTitle>
+        <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+        <Button>Button</Button>
+      </Card>
+    </CardColumns>
+  );
+};
 
 export default Projects;
