9534c6ddc584b3893218d0b452dd55a90fac869c
mihneavlad <mihnel@yahoo.com>
2018-09-05 16:39:50 +0200

    Working version with cards stacking properly

--- src/views/Projects.jsx
+++ src/views/Projects.jsx
@@ -1,28 +1,31 @@
 import React, {Fragment} from 'react';
 // import {Link} from "react-router-dom";
-import { Card, CardImg, CardTitle, CardText, CardColumns} from 'reactstrap';
+import { CardDeck, Col, Card, CardImg, CardTitle, CardText} from 'reactstrap';
 
 import projects from "../projects.json";
 
 
 const Projects = () => {
   return (
-    projects.map((myProject, index) =>
-      <Fragment>
-        <CardColumns>
-          <Card>
-            <CardImg
-              top width="100%"
-              src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180"
-              alt="Card image cap"
-              body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}
-            />
-            <CardTitle>{myProject.title}</CardTitle>
+    <Fragment>
+      <CardDeck>
+        {projects.map((myProject, index) =>
+
+          <Col sm="4">
+            <Card  key={index}>
+              <CardTitle>{myProject.title}</CardTitle>
+              <CardImg
+                top width="100%"
+                src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180"
+                alt="Card image cap"
+                body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}
+              />
+            </Card>
             <CardText>{myProject.description}</CardText>
-          </Card>
-        </CardColumns>
-      </Fragment>
-    )
+          </Col>
+        )}
+  </CardDeck>
+</Fragment>
   );
 };
 
